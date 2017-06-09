9ef476dd552ac21360ef8ffcbc61616e26255238
qqii <Qi-rui.Chen.2015@my.bristol.ac.uk>
2017-06-09 03:58:38 +0100

    Added TODO and selected thicker lines

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # [Harmonic Oscillator](https://qqii.github.io/harmonic-oscillator/)
 Simple experimentation with HTML5 canvas and Github's pages feature to make a cool looking animation.
+
+### TODO
+
+- [ ] Option to change variables
+- [ ] Better default variables
+- [ ] Generalise to multiple waves at once
+- [ ] Half section rendering
+- [ ] Select nice colors
--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 var canvas;
 var context;
 var step  = 0;
-var lwid  = 1;
-var lsep  = 8;
+var lwid  = 4;
+var lsep  = 20;
 var lines = 100;
 var ymax  = 200;
 
-var ratio = 0.7;
+var ratio = 0.9;
 // x, y for leftmost centre
 var xpos  = 0;
 var ypos  = 200;
@@ -37,8 +37,8 @@ var resize = function() {
   canvas.width  = window.innerWidth;
   canvas.height = window.innerHeight;
   lines = canvas.width / lsep;
-  ymax = canvas.height * 0.7;
-  ypos = canvas.height * 0.5;
+  ymax = canvas.height;
+  ypos = canvas.height / 2;
 }
 
 var clear = function() {
@@ -78,14 +78,14 @@ var redraw = function() {
 
   clear();
   for (var i = 0; i < lines; i++) {
-    drawLine(xpos + lsep * i, getY(i, step, 1, 0), 
-             xpos + lsep * i, getY(i, step, ratio, 10*Math.PI),
-             xpos + lsep * i, getY(i, step, ratio * ratio, 100*Math.PI),
-             xpos + lsep * i, getY(i, step, ratio * ratio * ratio, 1000*Math.PI)
+    drawLine(xpos + lsep * i, getY(i, step/1, 1, 0), 
+             xpos + lsep * i, getY(i, step/2, ratio, 10*Math.PI),
+             xpos + lsep * i, getY(i, step/4, ratio / 1.5, 100*Math.PI),
+             xpos + lsep * i, getY(i, step/8, ratio / 2, 1000*Math.PI)
             );
   }
 
-  step++
+  step++;
   requestAnimationFrame(redraw);
 }
 
